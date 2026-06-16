038aecd2c8ba4aff25224aeec6dedf5cad06cf57
Pablo Gomez <pabloagg20@gmail.com>
2026-06-16 12:04:25 -0500

    arreglo BASE_URL

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,5 @@
 export const API_CONFIG = {
-  //BASE_URL: 'https://backlogistica-production-c7a3.up.railway.app',
-  BASE_URL: 'http://192.168.1.84:3000',
+  BASE_URL: 'https://backlogistica-production-c7a3.up.railway.app',
   TIMEOUT: 60000,
 };
 
